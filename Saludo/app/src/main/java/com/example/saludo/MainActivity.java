9db270076a25029cdc0137fc06efb33ffd8848e5
chacon <zS20015721@estudiantes.uv.mx>
2023-12-07 13:17:50 -0600

    saludo

---
+++ Saludo/app/src/main/java/com/example/saludo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.saludo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.io.Console;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
